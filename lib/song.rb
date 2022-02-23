27a8f0350f4e061aa771e3a471d5b7f29b62ee4f
Patrick Liang <pl204511194@yahoo.com>
2022-02-23 13:40:14 -0500

    commit tree b54b75a7d78cbbbc42a9814b9d86a2eeba9f0262

---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+class Song
+    attr_reader :name, :artist, :genre
+    
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres.push(genre)
+        @@artists.push(artist)
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+    
+end
+
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+
+print x = Song.artist_count
